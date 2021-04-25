814c0b9a1fc181d8d27f525e0a8acbdce9133293
amanbhadauria01 <amanbhadauria01@gmail.com>
2021-04-25 15:57:26 +0530

    commit 1

---
+++ main.js
@@ -0,0 +1,27 @@
+let express = require("express");
+let app = express();
+let bodyparser = require("body-parser");
+let methodOverride = require("method-override");
+const fetch = require("node-fetch");
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+	res.render("landing");
+});
+
+app.post("/", function (req, res) {
+	let name = req.body.name;
+	fetch('https://www.superheroapi.com/api.php/2540844492728712/search/' + name)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data.results[0]);
+			res.render("show", { data: data.results[0] });
+		}
+		);
+});
+
+let port = 3000;
+app.listen(port, function () {
+	console.log("Server has Started");
+});
