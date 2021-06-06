bece05bf154addd28312f17ca3e0b6a241eb89a2
Anurag Garg <garanurag168@gmail.com>
2021-06-06 23:59:38 +0530

    Like dislike button basic

--- public/stylesheets/EventListeners.js
+++ public/stylesheets/EventListeners.js
@@ -1,6 +1,16 @@
-let buttons=documment.querySelectorAll(".lbutton");
-console.log(buttons[0]);
-buttons[0].addEventListener("click",()=>{
-    buttons[0].classList.add(".unselected");
-    buttons[1].classList.remove(".unselected");
-});
+// let buttons=document.querySelectorAll(".lbutton");
+// console.log(buttons[0]);
+// buttons[0].addEventListener("click",()=>{
+//     buttons[0].classList.add(".unselected");
+//     buttons[1].classList.remove(".unselected");
+// });
+// function init()
+// {
+//     console.log("123");
+// }
+
+exports.init=()=>{
+     console.log("Clicked");
+ }
+//console.log("Clicked");
+
